60cef9efae8f79908c260244c4797e60d072e0bb
emran <shuvoorba28@gmail.com>
2023-10-17 22:12:55 +0600

    error page added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import ErrorPage from './layouts/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div className=''>Hello world!</div>,
-    
+    errorElement: <ErrorPage></ErrorPage>
   },
 ]);
 
